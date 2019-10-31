2acd256f1b0e45b31ec84124b6513bcfa6386383
Rasfan M Faiz <rasfanfaiz@gmail.com>
2019-10-31 18:37:35 -0400

    build added

---
+++ build/precache-manifest.46918c97c2b25e99bd72bcea7add713c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e7139ef2bb111ce2cec05d8c0bae4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2f21e0764d6926f34c",
+    "url": "/static/css/main.4f1dab76.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "2c2f21e0764d6926f34c",
+    "url": "/static/js/main.72774e0c.chunk.js"
+  },
+  {
+    "revision": "1f1e643385e17360f83c",
+    "url": "/static/js/runtime-main.c2d72319.js"
+  }
+]);
